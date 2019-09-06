71e66a08c3c39e293ebe2e893ce1763572a3f0f0
liurongqing <phpgege@163.com>
2019-09-06 11:49:48 +0800

    1

--- src/router/admin.ts
+++ src/router/admin.ts
@@ -2,10 +2,17 @@ import * as Router from 'koa-router'
 const router = new Router()
 
 import loginController from '@/controller/login'
+import adminController from '@/controller/admin'
 // import activityController from '@/controller/activity'
 // import menuController from '@/controller/menu'
 
-router.post('/login', loginController.login).post('/logout', login.logout)
+router
+  .post('/login', loginController.login)
+  .post('/logout', loginController.logout)
+  .post('/checkLogin', loginController.checkLogin)
+  .get('/admin', adminController.find)
+  .post('/admin', adminController.save)
+  .delete('/admin', adminController.delete)
 // .get('/activity', activityController.find)
 // .post('/activity', activityController.save)
 // .delete('/activity', activityController.delete)
